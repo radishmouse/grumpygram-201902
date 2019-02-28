a82d2309ae49b9d93959644f50afe78f51306b44
Chris Aquino <chris.aquino@gmail.com>
2019-02-28 13:44:23 -0500

    adds css transition for each picture-frame

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,17 @@ const targetElements = document.querySelectorAll("[data-target]");
 // We see an enlarged version of image that is
 // inside that .picture-frame
 
-function respondToClick() {
+function respondToClick(event) {
     console.log('hello');
     console.log('how are you?');
     console.log('i am fine because i am a hamster.');
+
+    // console.log(event.target.parentElement);
+    console.log(event.target);
+    // add the "big" class to the thing that got clicked
+
+    // event.target.parentElement.classList.add('big');
+    event.target.classList.toggle('big');
 }
 
 function attachClickHandler(oneElement) {
@@ -20,3 +27,4 @@ function attachClickHandler(oneElement) {
 }
 
 targetElements.forEach(attachClickHandler);
+
